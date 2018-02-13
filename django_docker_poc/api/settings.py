983d1bf621cc8514993cca9d143634c20eb6c7e3
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-02-13 17:53:01 +0100

    add settings for api

---
+++ django_docker_poc/api/settings.py
@@ -0,0 +1,25 @@
+"""
+ * Copyright (C) 2017 Hendrik van Essen
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+"""
+
+path_root = "/RIOT/"
+
+module_directories = [
+    "sys",
+    "pkg",
+    "drivers"
+]
+
+application_directories = [
+    "examples"
+]
+
+LOGGING_FORMAT = "[%(levelname)s]: %(asctime)s\n"\
+                 + "in %(filename)s in %(funcName)s on line %(lineno)d\n"\
+                 + "%(message)s\n\n"
+
+APPLICATION_CACHE_DIR = ".application_cache"
