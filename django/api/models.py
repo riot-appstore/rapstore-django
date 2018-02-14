0a50979ee50baa420056dfe8d8584c5157696ea6
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-02-14 17:06:29 +0100

    remove User permissions from inherited User model

--- django/api/models.py
+++ django/api/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 import uuid
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    class Meta:
-        permissions = (('has_dev_perm','The current user has dev permissions'),)
 
 class Transaction(models.Model):
     uuid = models.UUIDField(default=uuid.uuid1, editable=False, unique=True)
