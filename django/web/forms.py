5b635df206ddfb94ca9ed8568082a146a77db5c0
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-02-21 13:28:35 +0100

    add fields to django form

--- django/web/forms.py
+++ django/web/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model=User
-        fields=('email',)
+        fields=('first_name', 'last_name', 'email',)
