9ceb34801a43a6bcadea7fc82cb989b8298d7cc8
danpetry <daniel.petry@fu-berlin.de>
2018-02-27 21:36:04 +0100

    uploader/views: form now saves uploaded file
    Added the new uploader model to the database in migrations
    folder, and the file now saves in the /apps directory with
    a database entry.

--- django/uploader/views.py
+++ django/uploader/views.py
@@ -1,13 +1,25 @@
 from django.shortcuts import render
 from django.template.loader import get_template
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from uploader.forms import UploadFileForm
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def uploader(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.author = request.user
+            obj.save()
+            return HttpResponseRedirect('/success/')
+        else:
+            return HttpResponseRedirect('/failure/')
+    else:
+        form = UploadFileForm()
+
     t = get_template('uploader.html')
-    form = UploadFileForm()
     html = t.render(context={"form": form}, request=request)
     return HttpResponse(html)
 
