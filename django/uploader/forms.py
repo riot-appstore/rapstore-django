6dbeb4ee221d5331706ac9406f5701abd2181e9c
danpetry <daniel.petry@fu-berlin.de>
2018-03-08 11:17:50 +0100

    Uploader: added migrations, and comments for logic
    Added some comments to indicate a possible implementation for a github
    URL submission, with automatic tar download. Either the URL or a direct
    tar upload would be available to the user, depending on radio buttons
    on the form.

--- django/uploader/forms.py
+++ django/uploader/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from uploader.models import Application
 
 class UploadFileForm(forms.ModelForm):
-    fields=('test_nonpopulated_field')
     class Meta:
         model=Application
         fields=('name', 'description', 'licences', 'project_page', 'app_folder', 'app_repo_url') 
