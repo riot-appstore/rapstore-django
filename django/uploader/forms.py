7c0e41334ccfa224926b2a73875141e325920a0e
danpetry <daniel.petry@fu-berlin.de>
2018-03-08 10:49:29 +0100

    Uploader: added fields to form and expanded model
    The form and underlying model now has app folder and app repo url
    as well as fields to enter licences and the project page.

--- django/uploader/forms.py
+++ django/uploader/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from uploader.models import Application
 
 class UploadFileForm(forms.ModelForm):
+    fields=('test_nonpopulated_field')
     class Meta:
         model=Application
-        fields=('elf_file', 'repo_url', 'name', 'description')
+        fields=('name', 'description', 'licences', 'project_page', 'app_folder', 'app_repo_url') 
