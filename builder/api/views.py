88e14b70f4f2c4f42fa9c6f193b537890356a7f2
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-03-12 13:33:21 +0100

    add builder files

---
+++ builder/api/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+import tempfile
+import tarfile
+import os
+from subprocess import Popen, PIPE, STDOUT
+
+def extract_tar(tar):
+    # Create temp folder
+    folder = tempfile.mkdtemp()
+    tar.extractall(path=folder)
+    return folder
+
+def write_tar(f):
+    try:
+        tf=tarfile.open(fileobj=f)
+    except:
+        return "Not OK"
+    return extract_tar(tf)
+
+def execute_makefile(app_build_dir, board, app_name):
+    """
+    Run make on given makefile and override variables
+    Parameters
+    ----------
+    app_build_dir: string
+        Path to makefile
+    board: string
+        Board name
+    app_name: string
+        Application name
+    Returns
+    -------
+    string
+        Output from executing make
+    """
+
+    cmd = ["make",
+           "-C", app_build_dir,
+           "RIOTBASE=/RIOT",
+           "BOARD=%s" % board]
+    #logging.debug('make: %s', cmd)
+
+    process = Popen(cmd, stdout=PIPE, stderr=STDOUT)
+    return process.communicate()[0]
+
+@csrf_exempt
+def test(request):
+    f = request.FILES['file']
+    dest=write_tar(f)
+
+    #Since we have the folder, let's do stuff
+    dest=execute_makefile(dest, "samr21-xpro", "test")
+
+    return HttpResponse(str(dest))
