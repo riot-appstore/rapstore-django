0afd3764ce510d1382f56cb200f91ffdcb18b58e
HendrikVE <hendrik1995@msn.com>
2018-03-29 14:51:30 +0200

    added install instruction page. fixed overlapping footer by workaround

---
+++ rapstore-frontend/src/app/page-install-instruction-browser-integration/page-install-instruction-browser-integration.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PageInstallInstructionBrowserIntegrationComponent } from './page-install-instruction-browser-integration.component';
+
+describe('PageInstallInstructionBrowserIntegrationComponent', () => {
+  let component: PageInstallInstructionBrowserIntegrationComponent;
+  let fixture: ComponentFixture<PageInstallInstructionBrowserIntegrationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PageInstallInstructionBrowserIntegrationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PageInstallInstructionBrowserIntegrationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
