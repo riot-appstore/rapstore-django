7d1d4723aefd1dda51dd8def51379eedf4ed7d7e
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-03-29 17:33:48 +0200

    frontend: implement file upload service

--- rapstore-frontend/src/app/app-uploader/app-uploader.component.ts
+++ rapstore-frontend/src/app/app-uploader/app-uploader.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-app-uploader',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app-uploader.component.css']
 })
 export class AppUploaderComponent implements OnInit {
-
-  constructor() { }
+  private file: Filelist;
+  model: any = {};
+  private baseurl = "http://localhost:8000";
+  constructor(private http: Http, private AuthService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  fileUpload() {
+  if(this.file && this.model.name) {
+    let formData:FormData = new FormData();
+    console.log(this.model.name);
+    formData.append('name', this.model.name;
+    formData.append('app_tarball', this.file, this.file.name);
+    let headers = new Headers();
+
+    headers.append('Authorization', 'Token '+this.AuthService.get_token());
+    headers.append('Accept', 'application/json');
+    let options = new RequestOptions({ headers: headers });
+    this.http.post(`${this.baseurl}/api/app/`, formData, options)
+        .map(res => res.json())
+        .subscribe(
+            data => console.log('success'),
+            error => console.log(error)
+        )
+  } 
+  }
+  fileChanged(event) {
+    this.file = event.target.files.length > 0 && event.target.files[0];
   }
 
 }
