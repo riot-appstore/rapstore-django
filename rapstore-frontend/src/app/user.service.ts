fa522788a8712601cf4838609b03ac708caf7642
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-04-03 14:39:46 +0200

    add user service

---
+++ rapstore-frontend/src/app/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, Output, EventEmitter } from '@angular/core';
+import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { AuthService } from './auth.service';
+import { User } from './models'
+
+@Injectable()
+export class UserService {
+  @Output() userChangeEvent: EventEmitter<User> = new EventEmitter(true);
+  private url = "http://localhost:8000/api/user/";
+  private contentType = {'Content-Type': 'application/json'};
+  private user: User;
+
+  constructor(private authService: AuthService, private http: Http) { }
+  refresh()
+  {
+    this.userChangeEvent.emit();
+  }
+  get() {
+    return this.http.get(this.url, this.getAuthOptions()).map(res => res.json());
+  }
+   getAuthOptions(): RequestOptions {
+     const authHeaders = new Headers(this.contentType);
+     authHeaders.append('Authorization', 'Token ' + this.authService.get_token());
+     return new RequestOptions({headers: authHeaders});
+  }
+
+}
