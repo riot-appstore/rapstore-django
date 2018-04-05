6128b0d51eaffac99fb40eaabf70cf7b1fe087b8
HendrikVE <hendrik1995@msn.com>
2018-04-05 14:39:41 +0200

    made app name field unique

--- django/api/models.py
+++ django/api/models.py
@@ -63,7 +63,7 @@ fs = FileSystemStorage(location='/apps')
 #Represents external applications (to be uploaded)
 class Application(models.Model):
     author = models.ForeignKey(User)
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(max_length=255, null=True, blank=True)
     licences = models.CharField(max_length=255, null=True, blank=True) 
     project_page = models.URLField(max_length=255, null=True, blank=True)
