1dafa3be2b147b139195a75070ecea98ebcd4b2e
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-04-19 17:27:57 +0200

    add proper environment variables

---
+++ rapstore-frontend/src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://api.dev.riot-apps.net"
+};
---
+++ rapstore-frontend/src/environments/environment.staging.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: "http://api.staging.riot-apps.net"
+};
