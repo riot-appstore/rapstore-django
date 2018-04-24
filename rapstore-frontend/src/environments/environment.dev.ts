aa86a6df6cc95e607a1d6493be8e99924a77cbdb
HendrikVE <hendrik1995@msn.com>
2018-04-24 16:57:11 +0200

    added app version and display version badge on website

--- rapstore-frontend/src/environments/environment.dev.ts
+++ rapstore-frontend/src/environments/environment.dev.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: "http://api.dev.riot-apps.net"
+    production: false,
+    apiUrl: "http://api.dev.riot-apps.net",
+    appVersion: "0.1.0 BETA"
 };
--- rapstore-frontend/src/environments/environment.prod.ts
+++ rapstore-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: true,
-  apiUrl: "http://api.riot-apps.net"
+    production: true,
+    apiUrl: "http://api.riot-apps.net",
+    appVersion: "0.1.0"
 };
