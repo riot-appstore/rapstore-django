67ba0fdbd26606e47098edd24b8e5e3b6fd14977
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-04-25 19:08:12 +0200

    parse missing attributes

--- rapstore-frontend/src/app/app-uploader/app-uploader.component.ts
+++ rapstore-frontend/src/app/app-uploader/app-uploader.component.ts
@@ -2,19 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Http, RequestOptions, Headers } from '@angular/http';
 import { AuthService } from '../auth.service';
 import { environment } from '../../environments/environment';
+import { Application } from '../models'
 
 @Component({
   selector: 'app-app-uploader',
   templateUrl: './app-uploader.component.html',
-  styleUrls: ['./app-uploader.component.css']
+  styleUrls: ['./app-uploader.component.css'],
+  providers: [Application]
 })
 export class AppUploaderComponent implements OnInit {
   private file: File;
-  model: any = {};
   message: string = "";
   error: string = "";
   private baseurl = environment.apiUrl;
-  constructor(private http: Http, private AuthService: AuthService) { }
+  constructor(private http: Http, private AuthService: AuthService, private model: Application) {
+    this.model.initial_instance={id: 0, version_name: "", version_code: ""}; }
 
   ngOnInit() {
   }
@@ -23,7 +25,12 @@ export class AppUploaderComponent implements OnInit {
   if(this.file && this.model.name) {
     let formData:FormData = new FormData();
     formData.append('name', this.model.name);
+    formData.append('description', this.model.description);
+    formData.append('licenses', this.model.licenses);
+    formData.append('project_page', this.model.project_page);
     formData.append('app_tarball', this.file, this.file.name);
+    formData.append('initial_instance.version_name', this.model.initial_instance.version_name);
+    formData.append('initial_instance.version_code', this.model.initial_instance.version_code);
     let headers = new Headers();
 
     headers.append('Authorization', 'Token '+this.AuthService.get_token());
