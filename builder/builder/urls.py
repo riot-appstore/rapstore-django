4b23967d0b02fb59929c46dc667ed3f8e8f28389
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-04-25 17:21:11 +0200

    add builder urls

--- builder/builder/urls.py
+++ builder/builder/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.conf.urls import url, include
 from rest_framework import routers
 from api.views import build
+from api.views import supported_boards
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^build/', build,{}, name='build'),
+    url(r'^supported_boards/', supported_boards,{}, name='supported_boards'),
 ]
