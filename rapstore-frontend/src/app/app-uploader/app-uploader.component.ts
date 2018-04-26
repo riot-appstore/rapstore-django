4965de262e8e7c86aa5d1aaa54bd98c684f86802
HendrikVE <hendrik1995@msn.com>
2018-04-26 13:30:43 +0200

    reformatted code

--- rapstore-frontend/src/app/app-uploader/app-uploader.component.ts
+++ rapstore-frontend/src/app/app-uploader/app-uploader.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { Http, RequestOptions, Headers } from '@angular/http';
-import { AuthService } from '../auth.service';
-import { environment } from '../../environments/environment';
-import { Application } from '../models'
+import {Component, OnInit} from '@angular/core';
+import {Http, RequestOptions, Headers} from '@angular/http';
+import {AuthService} from '../auth.service';
+import {environment} from '../../environments/environment';
+import {Application} from '../models';
 
 @Component({
   selector: 'app-app-uploader',
@@ -12,44 +12,47 @@ import { Application } from '../models'
 })
 export class AppUploaderComponent implements OnInit {
   private file: File;
-  message: string = "";
+  message: string = '';
   errors: string[] = [];
   private baseurl = environment.apiUrl;
+
   constructor(private http: Http, private AuthService: AuthService, protected model: Application) {
-    this.model.initial_instance={id: 0, version_name: "", version_code: ""}; }
+    this.model.initial_instance = {id: 0, version_name: '', version_code: ''};
+  }
 
   ngOnInit() {
   }
 
   fileUpload() {
-  if(this.file && this.model.name) {
-    this.errors = [];
-    let formData:FormData = new FormData();
-    formData.append('name', this.model.name);
-    formData.append('description', this.model.description);
-    formData.append('licenses', this.model.licenses);
-    formData.append('project_page', this.model.project_page);
-    formData.append('app_tarball', this.file, this.file.name);
-    formData.append('initial_instance.version_name', this.model.initial_instance.version_name);
-    formData.append('initial_instance.version_code', this.model.initial_instance.version_code);
-    let headers = new Headers();
+    if (this.file && this.model.name) {
+      this.errors = [];
+      let formData: FormData = new FormData();
+      formData.append('name', this.model.name);
+      formData.append('description', this.model.description);
+      formData.append('licenses', this.model.licenses);
+      formData.append('project_page', this.model.project_page);
+      formData.append('app_tarball', this.file, this.file.name);
+      formData.append('initial_instance.version_name', this.model.initial_instance.version_name);
+      formData.append('initial_instance.version_code', this.model.initial_instance.version_code);
+      let headers = new Headers();
 
-    headers.append('Authorization', 'Token '+this.AuthService.get_token());
-    headers.append('Accept', 'application/json');
-    let options = new RequestOptions({ headers: headers });
-    this.http.post(`${this.baseurl}/api/app/`, formData, options)
+      headers.append('Authorization', 'Token ' + this.AuthService.get_token());
+      headers.append('Accept', 'application/json');
+      let options = new RequestOptions({headers: headers});
+      this.http.post(`${this.baseurl}/api/app/`, formData, options)
         .map(res => res.json())
         .subscribe(
-        data => this.message = `Successfully uploaded your app "${this.model.name}" ! The app will be under a review process in order to make it public.`,
-        err => {
+          data => this.message = `Successfully uploaded your app "${this.model.name}" ! The app will be under a review process in order to make it public.`,
+          err => {
             let errors = JSON.parse(err.text());
             for (let k in errors) {
-                this.errors.push(`${k}: ${errors[k]}`);
+              this.errors.push(`${k}: ${errors[k]}`);
             }
-        }
-        )
+          }
+        );
+    }
   }
-  }
+
   fileChanged(event) {
     this.file = event.target.files.length > 0 && event.target.files[0];
   }
