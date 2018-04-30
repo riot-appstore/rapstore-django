cf9f6758df48bdceabdd11e51ed708541648b831
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-04-30 15:19:19 +0200

    Remove ApplicationInstance from admin

--- django/api/admin.py
+++ django/api/admin.py
@@ -57,7 +57,6 @@ class ApplicationAdmin(admin.ModelAdmin):
 # Register your models here.
 admin.site.register(Board, names(('internal_name',)))
 admin.site.register(Application, ApplicationAdmin)
-admin.site.register(ApplicationInstance, names(('application', 'is_public')))
 admin.site.register(Module, names(('name', 'description')))
 admin.site.register(Transaction, names(('uuid',)))
 admin.site.register(Feedback, names(('date', 'description',)))
