8194981c9707b7cb67288cefcb2b2a4c83a4a7b5
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-05-14 16:01:31 +0200

    add Tokens to board and app requests

--- rapstore-frontend/src/app/board.service.ts
+++ rapstore-frontend/src/app/board.service.ts
@@ -3,13 +3,14 @@ import {Http, Response, RequestOptions, Headers} from '@angular/http';
 import {Board} from './models';
 import {Observable} from 'rxjs/Observable';
 import {environment} from '../environments/environment';
+import {AuthService} from './auth.service';
 
 @Injectable()
 export class BoardService {
 
   private baseUrl = environment.apiUrl;
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private authService: AuthService) {
   }
 
   getAll(): Observable<Board[]> {
@@ -18,10 +19,15 @@ export class BoardService {
     return this.http.get(`${this.baseUrl}/api/board/`).map(res => res.json());
   }
 
+  getAuthOptions(): RequestOptions {
+    const authHeaders = new Headers();
+    authHeaders.append('Authorization', 'Token ' + this.authService.get_token());
+    authHeaders.append('Content-Type', 'application/json');
+    return new RequestOptions({headers: authHeaders});
+  }
   getSupported(app_id: number): Observable<Board[]> {
-    let headers = new Headers({'Content-Type': 'application/json'});
-    let options = new RequestOptions({headers: headers});
-    return this.http.get(`${this.baseUrl}/api/app/${app_id}/supported_boards/`).map(res => res.json());
+    let options = this.getAuthOptions();
+    return this.http.get(`${this.baseUrl}/api/app/${app_id}/supported_boards/`, options).map(res => res.json());
   }
 
 }
