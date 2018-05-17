c912a68c5344c4e0fafdbcaa9ad253ceff672023
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-05-17 13:41:41 +0200

    get SECRET_KEY from environment

--- builder/builder/settings.py
+++ builder/builder/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v^0nds6=g_b4$juu_!n^vh1$vt)o^9d!ro612ga6ryk3vt_%$w'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 env_dbg = os.getenv("DEBUG", None)
