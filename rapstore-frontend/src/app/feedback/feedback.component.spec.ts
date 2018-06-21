dc55d0340325b40fcaae520c93179da4471d8d80
HendrikVE <hendrik1995@msn.com>
2018-06-21 14:33:12 +0200

    fixed copy paste leftover

--- rapstore-frontend/src/app/feedback/feedback.component.spec.ts
+++ rapstore-frontend/src/app/feedback/feedback.component.spec.ts
@@ -2,7 +2,7 @@ import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {FeedbackComponent} from './feedback.component';
 
-describe('AppUploaderComponent', () => {
+describe('FeedbackComponent', () => {
   let component: FeedbackComponent;
   let fixture: ComponentFixture<FeedbackComponent>;
 
