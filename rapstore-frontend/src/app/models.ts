15971d66d144322fae6bfa906b3dbb1fc7b89b6c
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-07-10 17:14:29 +0200

    add form element models

--- rapstore-frontend/src/app/models.ts
+++ rapstore-frontend/src/app/models.ts
@@ -50,3 +50,38 @@ export class Signup {
 export class Feedback {
   description: string;
 }
+
+export class FormElementBase<T> {
+  value: T;
+  key: string;
+  label: string;
+  required: boolean;
+  order: number;
+  controlType: string;
+ 
+  constructor(options: {
+      value?: T,
+      key?: string,
+      label?: string,
+      required?: boolean,
+      order?: number,
+      controlType?: string
+    } = {}) {
+    this.value = options.value;
+    this.key = options.key || '';
+    this.label = options.label || '';
+    this.required = !!options.required;
+    this.order = options.order === undefined ? 1 : options.order;
+    this.controlType = options.controlType || '';
+  }
+}
+
+export class TextboxElement extends FormElementBase<string> {
+  controlType = 'textbox';
+  type: string;
+
+  constructor(options: {} = {}) {
+    super(options);
+    this.type = options['type'] || '';
+  }
+}
