13c442f2d01dcb4693d2ef2e6712efefd7469371
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-07-10 17:22:38 +0200

    implement FormElementComponent

--- rapstore-frontend/src/app/form-element/form-element.component.ts
+++ rapstore-frontend/src/app/form-element/form-element.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormElementBase } from '../models';
+import { FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-form-element',
   templateUrl: './form-element.component.html',
   styleUrls: ['./form-element.component.css']
 })
-export class FormElementComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class FormElementComponent {
+  @Input() element: FormElementBase<any>;
+  @Input() form: FormGroup;
+  // get isValid() { return this.form.controls[this.element.key].valid; }
 
 }
