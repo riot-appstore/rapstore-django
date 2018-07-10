d79c75e56997ac1f65c25ce53bbae1e9e1f29173
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-07-10 17:01:54 +0200

    add form element component

---
+++ rapstore-frontend/src/app/form-element/form-element.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-element',
+  templateUrl: './form-element.component.html',
+  styleUrls: ['./form-element.component.css']
+})
+export class FormElementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
