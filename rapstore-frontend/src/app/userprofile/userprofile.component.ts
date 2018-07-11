20b8835100aa46f7970ad51905d68a4800d07a9c
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-07-11 17:31:15 +0200

    reimplement user profile form

--- rapstore-frontend/src/app/userprofile/userprofile.component.ts
+++ rapstore-frontend/src/app/userprofile/userprofile.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../user.service';
 import {User} from '../models';
+import {FormElementBase, TextboxElement} from '../models';
+import { FormGroup } from '@angular/forms';
+import {DynFormService} from '../dyn-form.service';
 
 @Component({
   selector: 'app-userprofile',
@@ -8,24 +11,35 @@ import {User} from '../models';
   styleUrls: ['./userprofile.component.css']
 })
 export class UserprofileComponent implements OnInit {
-  private model: any = {};
   private edit: boolean = false;
   private user: User;
+  form: FormGroup;
+  elements: FormElementBase<any>[];
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private df: DynFormService) {
+    this.elements = [
+      new TextboxElement({key: "first_name", label: "First name"}),
+      new TextboxElement({key: "last_name", label: "Last name"}),
+      new TextboxElement({key: "location", label: "Location"}),
+      new TextboxElement({key: "company", label: "Company"}),
+      new TextboxElement({key: "gender", label: "Gender"}),
+      new TextboxElement({key: "phone_number", label: "Phone number"}),
+    ];
+    this.form = df.toFormGroup(this.elements);
   }
 
   ngOnInit() {
-    this.userService.get().subscribe(user => this.user = user);
+    this.userService.get().subscribe(user => {this.user = user; this.form.patchValue(user)});
   }
 
   update() {
-    this.userService.update(this.model).subscribe(res => this.set_edit(false));
+    this.userService.update(this.form.value).subscribe(res => this.set_edit(false));
   }
 
   set_edit(enable: boolean) {
+    this.user = this.form.value;
     if (enable) {
-      this.model = this.user;
+      this.form.patchValue(this.user);
     }
     this.edit = enable;
   }
